2b05987a9e6a1cdd56308b2f506430a35887eed9
Franco Dall'Ongaro <67251059+francodallongaro@users.noreply.github.com>
2021-01-06 20:20:21 -0300

    cambios en requirements

--- hello/models.py
+++ hello/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
 
 
 # PLANES
