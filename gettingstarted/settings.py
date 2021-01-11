1c177192a53fa1dc59bcc465ee57460158c4cd7f
Franco Dall'Ongaro <67251059+francodallongaro@users.noreply.github.com>
2021-01-11 19:43:10 -0300

    database aws

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE" : "django.db.backends.postgresql",
-#         "NAME": "database-ideal",
-#         "USER": "idealdatabase",
-#         "PASSWORD": "2IdealServicios",
-#         "HOST": "database-ideal.c8xhkoscqibi.us-east-2.rds.amazonaws.com",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE" : "django.db.backends.postgresql",
+        "NAME": "idealdb",
+        "USER": "idealdatabase",
+        "PASSWORD": "2IdealServicios",
+        "HOST": "idealdb.c8xhkoscqibi.us-east-2.rds.amazonaws.com",
+        "PORT": "5432",
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -99,9 +99,9 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # import dj_database_url
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 # DATABASES = {
 #     'default': {
