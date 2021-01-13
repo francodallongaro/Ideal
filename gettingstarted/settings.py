643a43d7544e9ce14ed8594c5a6d3de6c3afa112
Franco Dall'Ongaro <67251059+francodallongaro@users.noreply.github.com>
2021-01-13 00:53:19 -0300

    prueba de nuevo

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -41,9 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "hello",
-
     "storages",
+    "hello",
 ]
 
 MIDDLEWARE = [
