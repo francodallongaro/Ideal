2b92181aab85817bbe9765dd1ecb1d59e386eb7e
Franco Dall'Ongaro <67251059+francodallongaro@users.noreply.github.com>
2021-01-26 10:10:05 -0300

    collect

--- hello/storage_backends.py
+++ hello/storage_backends.py
@@ -1,9 +1,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
-class StaticStorage(S3Boto3Storage):
-    location = 'static'
-    default_acl = 'public'
+# class StaticStorage(S3Boto3Storage):
+#     location = 'static'
+#     default_acl = 'public'
     
 
 class MediaStorage(S3Boto3Storage):
